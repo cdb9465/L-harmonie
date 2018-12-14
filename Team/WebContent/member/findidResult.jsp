63f251ff6584cced47544f38f4d83b759ddb61ff
ITWILL <ITWILL@ITWILLBS02-26>
2018-12-14 17:33:16 +0900

    담당페이지 최종 완료 _박소정

--- Team/WebContent/member/findidResult.jsp
+++ Team/WebContent/member/findidResult.jsp
@@ -1,3 +1,5 @@
+<%@page import="java.util.Vector"%>
+<%@page import="java.util.Arrays"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -20,16 +22,16 @@
 <!-- 헤더파일들어가는 곳 -->
 <div class="clear"></div>
 <%request.setCharacterEncoding("utf-8");
-String findEmail = (String)request.getAttribute("findEmail");
-
+//아이디의 앞3자리만 표시된 값 s1
+String s1 = (String)request.getAttribute("s1");
 %>
 <div id="article_fi">
 		<!-- <div id="backimg">
 		이미지영역
 		</div> -->
 	<div id="inputForm">
-			<div id="resultId">회원님의 아이디는 <br><span><%=findEmail%></span> 입니다.</div>
-			<input type="button" value="비밀번호 찾기" id="resultbtn1" onclick="location.href='./FindIdPass.me?id=passck'">
+			<div id="resultId">회원님의 아이디는 <br><span><%=s1%></span> 입니다.</div>
+			<input type="button" value="비밀번호 찾기" id="resultbtn1" onclick="location.href='./FindIdPass.me?a=passOpen'">
 		<input type="button" value="홈으로 이동" id="resultbtn" onclick="location.href='./Main.ma'">
 		
 
--- Team/src/net/member/action/FindIdAction.java
+++ Team/src/net/member/action/FindIdAction.java
@@ -1,6 +1,8 @@
 package net.member.action;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,7 +24,9 @@ public class FindIdAction implements Action{
 		String findEmail = mdao.findId(name, phone);
 
 
+
 		if(findEmail==null){		
+//값이 없으면 메세지출력 후 페이지로 되돌아간다
 			//자바에서 JSP구문 쓸때 ↓ 아래 형식으로 사용
 			response.setContentType("text/html; charset=UTF-8");	
 			PrintWriter out = response.getWriter();
@@ -33,8 +37,25 @@ public class FindIdAction implements Action{
 			out.close();		
 			return null;
 		}else{
-			//결과값 내보내기
-			request.setAttribute("findEmail",findEmail);
+//값이 있으면 
+			//findEmail값을 하나씩 잘라서 email에 넣는다
+			String[] email = findEmail.split("");
+			//값의 0,1,2번째 자리를 제외한 나머지 *로 표시
+			for(int i=0;i<email.length;i++){
+				if(i>2){ email[i]="*"; }
+			}
+			System.out.println("결과1111!:"+Arrays.toString(email));
+			
+			//s1 값 안에 email배열 누적해서 넣기
+			String s1 = "";
+			for(int i=0;i<email.length;i++){
+				s1 = s1+(String)email[i];
+			}
+			System.out.println("결과2222!:"+s1);
+			
+			//최종 결과값 내보내기
+			request.setAttribute("s1",s1);
+			
 			//forward 객체생성
 			ActionForward forward=new ActionForward();
 			forward.setRedirect(false); //jsp로 가면 false, 가상주소로 가면 true 이다.
